6aefb8bdbb330be84d502116baff72612a4d4a92
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-01-29 23:39:02 +0700

    Update github workflow

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import About from '@/views/About.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
+describe('About.vue', () => {
+  it('renders LIFF title', () => {
+    const msg = 'LIFF';
+    const wrapper = shallowMount(About);
     expect(wrapper.text()).toMatch(msg);
   });
 });
