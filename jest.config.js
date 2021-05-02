f3046d5a740dec85c1895cc355ac50c6181bc937
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-05-02 13:09:05 +0700

    Fix unit test problem

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{ts,vue}', '!src/main.ts']
+  collectCoverageFrom: ['src/**/*.{ts,vue}', '!src/main.ts'],
+  moduleFileExtensions: ["js","ts","json","vue"]
 };
